6f326d122df662d038377e1d6b3cb25521b1f2dd
Alina <alinarosi169@gmail.com>
2026-05-02 16:47:17 +0200

    backend + db containerizzati con docker compose funzionante

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,16 +1,15 @@
 import dotenv from "dotenv";
-
-dotenv.config({ path: ".env" });
+dotenv.config();
 
 import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST || "localhost",
+  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
+  user: process.env.DB_USER || "postgres",
+  password: process.env.DB_PASSWORD || "postgres",
+  database: process.env.DB_NAME || "taskdb",
 });
 
 export default pool;
